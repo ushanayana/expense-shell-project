f10b52a22de22dd42846a769bf4e638700cb150a
ushanayana <ushanayana@gmail.com>
2026-02-15 03:51:23 +0530

    project

--- mysql.sh
+++ mysql.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 USERID=$(id -u)
-TIMESTAMP=(date +%F-%H-%M-%S)
-SCRIPT_NAME=(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 R="\e[31m"
 G="\e[32m"
