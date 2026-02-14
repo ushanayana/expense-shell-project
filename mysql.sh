b7d3d8d8721fb1519b44a5b68ebc7054a1616e1d
ushanayana <ushanayana@gmail.com>
2026-02-15 03:42:53 +0530

    project

--- mysql.sh
+++ mysql.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
 USERID=$(id -u)
-
+TIMESTAMP=(date +%F-%H-%M-%S)
+SCRIPT_NAME=(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 VALIDATE(){
-    if [ $1 eq 0 ]
-    then
-        echo "$2 installation is success..."
-    else    
-        echo "$2 installation is failure....."
-        exit
-    fi         
+    if [ $1 -eq 0 ]
+    then 
+        echo -e "$G $2 is success.....$N"
+    else 
+        echo -e "$R $2 is Failure.....$N"
+        exit    
 }
-
 
 
 if [ $USERID -ne 0 ]
 then
-    echo "you r not a super user"
+    echo -e "$Y you r not a super user $N"
     exit 1
 else
-    echo "you r super user"
+    echo -e "$R you r super user $N"
 fi 
+
+dnf install mysql -y &>>LOGFILE
+VALIDATE $? "Installing mysql"
 
